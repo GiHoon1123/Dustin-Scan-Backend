55651a0ec13c9fb5019875eabefc0a6ffe929bdd
GiWoonHwang <85157729+GiWoonHwang@users.noreply.github.com>
2025-10-28 14:45:12 +0900

    Update shared module configuration

--- shared/shared.module.ts
+++ shared/shared.module.ts
@@ -1,10 +1,15 @@
 import { Global, Module } from '@nestjs/common';
+import { ChainClientModule } from './chain-client';
 
+/**
+ * 전역 공유 모듈
+ *
+ * @Global 데코레이터로 전역 모듈로 선언
+ * 모든 앱에서 import 없이 사용 가능
+ */
 @Global()
 @Module({
-  imports: [],
-  providers: [],
-  exports: [],
+  imports: [ChainClientModule],
+  exports: [ChainClientModule],
 })
 export class SharedModule {}
-
